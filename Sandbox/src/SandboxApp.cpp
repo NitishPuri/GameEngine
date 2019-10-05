e3af71da430973cf0852311a9b798657237082f9
Puri, Nitish <nitish.puri@stryker.com>
2019-10-06 05:10:45 +0530

    adds application sandbox.

---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,17 @@
+
+#include <GameEngine.h>
+
+class Sandbox : public GE::Application 
+{
+public:
+  Sandbox()
+  {}
+
+  ~Sandbox()
+  {}
+};
+
+GE::Application* GE::CreateApplication()
+{
+  return new Sandbox();
+}
