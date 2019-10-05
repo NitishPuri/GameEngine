67571d281aa5776fbde24fcbba5b5ca84cd03895
Puri, Nitish <nitish.puri@stryker.com>
2019-10-06 05:10:13 +0530

    adds game engine base include.

---
+++ GameEngine/src/GameEngine.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// For use by GameEngine applications
+
+#include "GameEngine/Application.h"
+
+
+
+// ---- Entry Point -------------------------------
+#include "GameEngine/EntryPoint.h"
+// ---- Entry Point -------------------------------
