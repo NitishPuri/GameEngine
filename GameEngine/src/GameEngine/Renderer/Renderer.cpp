7fec737dbacc0fe7cb1d30c0d5fde96d81b0d9c9
Puri, Nitish <nitish.puri@stryker.com>
2019-10-07 04:57:24 +0530

    adds renderer api and buffer objects.

---
+++ GameEngine/src/GameEngine/Renderer/Renderer.cpp
@@ -0,0 +1,6 @@
+#include "gepch.h"
+#include "Renderer.h"
+
+namespace GE {
+  RendererAPI Renderer::s_RendererAPI = RendererAPI::OpenGL;
+}
---
+++ GameEngine/src/Platform/OpenGL/OpenGLBuffer.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "GameEngine/Renderer/Buffer.h"
+
+namespace GE {
+  class OpenGLVertexBuffer : public VertexBuffer {
+  public:
+    OpenGLVertexBuffer(float* vertices, uint32_t size);
+    virtual ~OpenGLVertexBuffer();
+
+    void Bind() const override;
+    void Unbind() const override;
+
+  private:
+    uint32_t m_RendererID;
+  };
+
+  class OpenGLIndexBuffer : public IndexBuffer {
+  public:
+    OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
+    virtual ~OpenGLIndexBuffer();
+
+    void Bind() const override;
+    void Unbind() const override;
+
+    uint32_t GetCount() const override { return m_Count; }
+
+  private:
+    uint32_t m_RendererID;
+    uint32_t m_Count;
+  };
+
+}
