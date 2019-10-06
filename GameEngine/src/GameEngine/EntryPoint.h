89ae01f063fbde4d38730a8b3995fc157d6bd514
Puri, Nitish <nitish.puri@stryker.com>
2019-10-06 06:13:53 +0530

    adds logging class

--- GameEngine/src/GameEngine/EntryPoint.h
+++ GameEngine/src/GameEngine/EntryPoint.h
@@ -6,6 +6,10 @@ extern GE::Application* GE::CreateApplication();
 
 int main(int argc, char** argv)
 {
+  GE::Log::Init();  
+  GE_CORE_WARN("Initialized Log!");
+  GE_INFO("Hello!");
+
   auto app = GE::CreateApplication();
   app->Run();
   delete app;
