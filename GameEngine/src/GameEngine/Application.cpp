8fcdeb80ac2642ad976b67325954c32acf64a61b
Puri, Nitish <nitish.puri@stryker.com>
2019-10-06 19:27:48 +0530

    Adds glfw event callbacks and dispatch window close event.

--- GameEngine/src/GameEngine/Application.cpp
+++ GameEngine/src/GameEngine/Application.cpp
@@ -7,9 +7,12 @@
 
 namespace GE {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
   Application::Application()
   {
     m_Window = std::unique_ptr<Window>(Window::Create());
+    m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
   }
 
 
@@ -24,6 +27,20 @@ namespace GE {
       glClear(GL_COLOR_BUFFER_BIT);
       m_Window->OnUpdate();
     }
+  }
+
+  void Application::OnEvent(Event & e)
+  {
+    EventDispatcher dispatcher(e);
+    dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+    GE_CORE_TRACE("{0}", e);
+  }
+
+  bool Application::OnWindowClose(WindowCloseEvent & e)
+  {
+    m_Running = false;
+    return true;
   }
 
 }
--- GameEngine/src/GameEngine/Application.h
+++ GameEngine/src/GameEngine/Application.h
@@ -3,6 +3,8 @@
 #include "Core.h"
 #include "Window.h"
 
+#include "Events/ApplicationEvent.h"
+
 namespace GE {
 
   class GE_API Application
@@ -12,7 +14,11 @@ namespace GE {
     virtual ~Application();
 
     void Run();
+
+    void OnEvent(Event& e);
   private:
+    bool OnWindowClose(WindowCloseEvent& e);
+
     std::unique_ptr<Window> m_Window;
     bool m_Running = true;
   };
