26c51bee75837afdfba219108005fbec334d4588
Puri, Nitish <nitish.puri@stryker.com>
2019-10-06 23:46:18 +0530

    enable asserts in debug mode.

--- GameEngine/src/GameEngine/Core.h
+++ GameEngine/src/GameEngine/Core.h
@@ -10,9 +10,13 @@
   #error Game Engine only supports windows
 #endif // GE_PLATFORM_WINDOWS
 
+#ifdef GE_DEBUG
+  #define GE_ENABLE_ASSERTS
+#endif
+
 #ifdef GE_ENABLE_ASSERTS
-  #define GE_ASSERT(x, ...) { if(!(x)) { GE_ERROR("Asserion Failed: {0}", __VA_ARGS); __debug_break(); } }
-  #define GE_CORE_ASSERT(x, ...) { if(!(x)) { GE_CORE_ERROR("Asserion Failed: {0}", __VA_ARGS); __debug_break(); } }
+  #define GE_ASSERT(x, ...) { if(!(x)) { GE_ERROR("Asserion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+  #define GE_CORE_ASSERT(x, ...) { if(!(x)) { GE_CORE_ERROR("Asserion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 #else // GE_ENABLE_ASSERTS
   #define GE_ASSERT(x, ...) 
   #define GE_CORE_ASSERT(x, ...) 
