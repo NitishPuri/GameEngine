99a223d1377ad2124d2b354d715928e9747f448a
Puri, Nitish <nitish.puri@stryker.com>
2019-10-07 00:01:23 +0530

    Add input class for polling input.

--- GameEngine/src/Platform/Windows/WindowsWindow.h
+++ GameEngine/src/Platform/Windows/WindowsWindow.h
@@ -24,6 +24,8 @@ namespace GE {
     void SetVSync(bool enabled) override;
     bool IsVSync() const override;
 
+    inline virtual void* GetNativeWindow() const { return m_Window; }
+
   private:
 
     virtual void Init(const WindowProps& props);
