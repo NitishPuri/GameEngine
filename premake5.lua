29da3048f4dc911b537cca7767b144581beb6f05
Puri, Nitish <nitish.puri@stryker.com>
2019-10-07 00:02:08 +0530

    update premake

--- premake5.lua
+++ premake5.lua
@@ -28,6 +28,7 @@ project "GameEngine"
   location  "GameEngine"
   kind "SharedLib"
   language "C++"
+  staticruntime "off"
 
   targetdir ("bin/" .. outputdir .. "/%{prj.name}")
   objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -57,7 +58,6 @@ project "GameEngine"
 
   filter "system:windows"
     cppdialect "C++17"
-    staticruntime "On"
     systemversion "latest"
 
       defines {
@@ -72,17 +72,17 @@ project "GameEngine"
 
   filter "configurations:Debug"
     defines "GE_DEBUG"
-    buildoptions "/MDd"
+    runtime "Debug"
     symbols "On"
 
   filter "configurations:Release"
     defines "GE_RELEASE"
-    buildoptions "/MD"
+    runtime "Release"
     optimize "On"
 
   filter "configurations:Dist"
     defines "GE_DIST"
-    buildoptions "/MD"
+    runtime "Release"
     optimize "On"
   
 
@@ -90,6 +90,7 @@ project "Sandbox"
   location  "Sandbox"
   kind "ConsoleApp"
   language "C++"
+  staticruntime "off"
 
   targetdir ("bin/" .. outputdir .. "/%{prj.name}")
   objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -110,7 +111,6 @@ project "Sandbox"
 
   filter "system:windows"
     cppdialect "C++17"
-    staticruntime "On"
     systemversion "latest"
 
       defines {
@@ -119,15 +119,15 @@ project "Sandbox"
 
   filter "configurations:Debug"
     defines "GE_DEBUG"
-    buildoptions "/MDd"
+    runtime "Debug"
     symbols "On"
 
   filter "configurations:Release"
     defines "GE_RELEASE"
-    buildoptions "/MD"
+    runtime "Release"
     optimize "On"
 
   filter "configurations:Dist"
     defines "GE_DIST"
-    buildoptions "/MD"
+    runtime "Release"
     optimize "On"
