1b3b147b98172dcff5e262945dd1ba45fe123631
Puri, Nitish <nitish.puri@stryker.com>
2019-10-06 20:20:12 +0530

    adds layer example.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,11 +1,34 @@
 
 #include <GameEngine.h>
 
+class ExampleLayer : public GE::Layer
+{
+public:
+  ExampleLayer()
+    : Layer("Example")
+  {
+  }
+
+  void OnUpdate() override
+  {
+    GE_INFO("ExampleLayer::Update");
+  }
+
+  void OnEvent(GE::Event& event) override
+  {
+    GE_TRACE("{0}", event);
+  }
+
+};
+
+
 class Sandbox : public GE::Application 
 {
 public:
   Sandbox()
-  {}
+  {
+    PushLayer(new ExampleLayer());
+  }
 
   ~Sandbox()
   {}
