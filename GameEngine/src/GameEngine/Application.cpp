ed1392e3185fcbbd4c780cf75b2698a9c287e315
Puri, Nitish <nitish.puri@stryker.com>
2019-10-07 01:40:54 +0530

    onAttach is called by layerstack

--- GameEngine/src/GameEngine/Application.cpp
+++ GameEngine/src/GameEngine/Application.cpp
@@ -63,13 +63,11 @@ namespace GE {
   void Application::PushLayer(Layer * layer)
   {
     m_LayerStack.PushLayer(layer);
-    layer->OnAttach();
   }
 
   void Application::PushOverlay(Layer * overlay)
   {
     m_LayerStack.PushOverlay(overlay);
-    overlay->OnAttach();
   }
 
   bool Application::OnWindowClose(WindowCloseEvent & e)
--- GameEngine/src/GameEngine/LayerStack.cpp
+++ GameEngine/src/GameEngine/LayerStack.cpp
@@ -15,12 +15,14 @@ namespace GE {
   void LayerStack::PushLayer(Layer * layer)
   {
     m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, layer);
+    layer->OnAttach();
     m_LayerInsertIndex++;
   }
 
   void LayerStack::PushOverlay(Layer * overlay)
   {
     m_Layers.emplace_back(overlay);
+    overlay->OnAttach();
   }
 
   void LayerStack::PopLayer(Layer * layer)
@@ -29,7 +31,9 @@ namespace GE {
     if (it != m_Layers.end()) {
       m_Layers.erase(it);
       m_LayerInsertIndex--;
+      layer->OnDetach();
     }
+
   }
 
   void LayerStack::PopOverlay(Layer * overlay)
@@ -37,6 +41,7 @@ namespace GE {
     auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
     if (it != m_Layers.end()) {
       m_Layers.erase(it);
+      overlay->OnDetach();
     }
   }
 
