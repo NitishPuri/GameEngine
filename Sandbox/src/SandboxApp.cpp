cf5a7477a61bf01aa7d55bb3465bb6cfc8ca0b33
Puri, Nitish <nitish.puri@stryker.com>
2019-10-07 08:36:27 +0530

    move opengl shader implementation to OpenGLShader class

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -4,6 +4,9 @@
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "Platform/OpenGL/OpenGLShader.h"
 
 class ExampleLayer : public GE::Layer
 {
@@ -91,7 +94,7 @@ public:
 
     m_Shader.reset(GE::Shader::Create(vertexSrc, fragmentSrc));
 
-    std::string blueShaderVertexSrc = R"(
+    std::string flatColorShaderVertexSrc = R"(
       #version 330 core
       
       layout(location = 0) in vec3 a_Position;
@@ -107,18 +110,21 @@ public:
       }
     )";
 
-    std::string blueShaderFragmentSrc = R"(
+    std::string flatColorShaderFragmentSrc = R"(
       #version 330 core
       
       layout(location = 0) out vec4 color;
       in vec3 v_Position;
+
+      uniform vec3 u_Color;
+
       void main()
       {
-        color = vec4(0.2, 0.3, 0.8, 1.0);
+        color = vec4(u_Color, 1.0);
       }
     )";
 
-    m_BlueShader.reset(GE::Shader::Create(blueShaderVertexSrc, blueShaderFragmentSrc));
+    m_FlatColorShader.reset(GE::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 
   }
 
@@ -149,13 +155,16 @@ public:
 
     glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+    std::dynamic_pointer_cast<GE::OpenGLShader>(m_FlatColorShader)->Bind();
+    std::dynamic_pointer_cast<GE::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
     for (int y = 0; y < 20; y++)
     {
       for (int x = 0; x < 20; x++)
       {
         glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
         glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-        GE::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+        GE::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
       }
     }
 
@@ -168,6 +177,7 @@ public:
   {
     ImGui::Begin("Test");
     ImGui::Text("Hello World");
+    ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
     ImGui::End();
   }
 
@@ -180,7 +190,7 @@ private:
   std::shared_ptr<GE::Shader> m_Shader;
   std::shared_ptr<GE::VertexArray> m_VertexArray;
 
-  std::shared_ptr<GE::Shader> m_BlueShader;
+  std::shared_ptr<GE::Shader> m_FlatColorShader;
   std::shared_ptr<GE::VertexArray> m_SquareVA;
 
   GE::OrthographicCamera m_Camera;
@@ -190,6 +200,7 @@ private:
   float m_CameraRotation = 0.0f;
   float m_CameraRotationSpeed = 180.0f;
 
+  glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 
