e91fa63de9d5d62df682dbc0f2c6e614c3bd725b
Puri, Nitish <nitish.puri@stryker.com>
2019-10-07 06:51:55 +0530

    add rendererApi and renderCommand abstractions

---
+++ GameEngine/src/GameEngine/Renderer/RenderCommand.cpp
@@ -0,0 +1,8 @@
+#include "gepch.h"
+#include "RenderCommand.h"
+
+#include "Platform/OpenGL/OpenGLRendererAPI.h"
+
+namespace GE {
+  RendererAPI* RenderCommand::s_RendererAPI = new OpenGLRendererAPI;
+}
---
+++ GameEngine/src/GameEngine/Renderer/RendererAPI.cpp
@@ -0,0 +1,6 @@
+#include "gepch.h"
+#include "RendererAPI.h"
+
+namespace GE {
+  RendererAPI::API RendererAPI::s_API = RendererAPI::API::OpenGL;
+}
