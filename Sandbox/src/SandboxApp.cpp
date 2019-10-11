8c22a787303097f0884ecdb7d17bcc9164768e41
Puri, Nitish <nitish.puri@stryker.com>
2019-10-12 01:46:34 +0530

    texture example.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -40,18 +40,20 @@ public:
 
     m_SquareVA.reset(GE::VertexArray::Create());
 
-    float squareVertices[3 * 4] = {
-      -0.5f, -0.5f, 0.0f,
-       0.5f, -0.5f, 0.0f,
-       0.5f,  0.5f, 0.0f,
-      -0.5f,  0.5f, 0.0f,
+    float squareVertices[5 * 4] = {
+      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+       0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+       0.5f,  0.5f, 0.0f, 1.0f, 1.0f,
+      -0.5f,  0.5f, 0.0f, 0.0f, 1.0f
     };
 
     GE::Ref<GE::VertexBuffer> squareVB;
     squareVB.reset(GE::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
-    squareVB->SetLayout({{ 
-        GE::ShaderDataType::Float3, "a_Position" }
-      });
+    squareVB->SetLayout({
+      { GE::ShaderDataType::Float3, "a_Position" },
+      { GE::ShaderDataType::Float2, "a_TexCoord" }
+    });
+
     m_SquareVA->AddVertexBuffer(squareVB);
 
     uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
@@ -126,6 +128,41 @@ public:
 
     m_FlatColorShader.reset(GE::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 
+    std::string textureShaderVertexSrc = R"(
+      #version 330 core
+      
+      layout(location = 0) in vec3 a_Position;
+      layout(location = 1) in vec2 a_TexCoord;
+      uniform mat4 u_ViewProjection;
+      uniform mat4 u_Transform;
+      out vec2 v_TexCoord;
+      void main()
+      {
+        v_TexCoord = a_TexCoord;
+        gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);  
+      }
+    )";
+
+    std::string textureShaderFragmentSrc = R"(
+      #version 330 core
+      
+      layout(location = 0) out vec4 color;
+      in vec2 v_TexCoord;
+      
+      uniform sampler2D u_Texture;
+      void main()
+      {
+        color = texture(u_Texture, v_TexCoord);
+      }
+    )";
+
+    m_TextureShader.reset(GE::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
+
+    m_Texture = GE::Texture2D::Create("assets/textures/logo2.png");
+
+    std::dynamic_pointer_cast<GE::OpenGLShader>(m_TextureShader)->Bind();
+    std::dynamic_pointer_cast<GE::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
+
   }
 
   void OnUpdate(GE::Timestep ts) override
@@ -167,10 +204,8 @@ public:
 
     std::dynamic_pointer_cast<GE::OpenGLShader>(m_FlatColorShader)->Bind();
 
-    for (int y = 0; y < 20; y++)
-    {
-      for (int x = 0; x < 20; x++)
-      {
+    for (int y = 0; y < 20; y++) {
+      for (int x = 0; x < 20; x++) {
         if ( (x + y) % 2 == 0) {
           std::dynamic_pointer_cast<GE::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
         }
@@ -183,7 +218,10 @@ public:
       }
     }
 
-    GE::Renderer::Submit(m_Shader, m_VertexArray);
+    //GE::Renderer::Submit(m_Shader, m_VertexArray);
+
+    m_Texture->Bind();
+    GE::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
     GE::Renderer::EndScene();
   }
@@ -208,6 +246,9 @@ private:
 
   GE::Ref<GE::Shader> m_FlatColorShader;
   GE::Ref<GE::VertexArray> m_SquareVA;
+
+  GE::Ref<GE::Shader> m_TextureShader;
+  GE::Ref<GE::Texture> m_Texture;
 
   GE::OrthographicCamera m_Camera;
   glm::vec3 m_CameraPosition;
