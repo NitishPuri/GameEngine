df14d9954b0461ef5feab4f5018e9025e1703fca
Puri, Nitish <nitish.puri@stryker.com>
2019-10-12 01:46:44 +0530

    add stb to premake.

--- premake5.lua
+++ premake5.lua
@@ -18,6 +18,7 @@ IncludeDir["GLFW"] = "GameEngine/vendor/GLFW/include"
 IncludeDir["Glad"] = "GameEngine/vendor/Glad/include"
 IncludeDir["ImGui"] = "GameEngine/vendor/imgui"
 IncludeDir["glm"] = "GameEngine/vendor/glm"
+IncludeDir["stb_image"] = "GameEngine/vendor/stb_image"
 
 group "Dependencies"
   include "GameEngine/vendor/GLFW"
@@ -41,6 +42,8 @@ project "GameEngine"
   files {
     "%{prj.name}/src/**.h",
     "%{prj.name}/src/**.cpp",
+    "%{prj.name}/vendor/stb_image/**.h",
+    "%{prj.name}/vendor/stb_image/**.cpp",
     "%{prj.name}/vendor/glm/glm/**.hpp",
     "%{prj.name}/vendor/glm/glm/**.inl"
   }
@@ -55,7 +58,8 @@ project "GameEngine"
     "%{IncludeDir.GLFW}",
     "%{IncludeDir.Glad}",
     "%{IncludeDir.ImGui}",
-    "%{IncludeDir.glm}"
+    "%{IncludeDir.glm}",
+    "%{IncludeDir.stb_image}"
   }
 
   links{
