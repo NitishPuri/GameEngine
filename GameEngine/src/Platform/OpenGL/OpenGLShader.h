3d69df512fe4ef322cb0adfa97d8caa8f1dcef72
Puri, Nitish <nitish.puri@stryker.com>
2019-10-14 03:06:18 +0530

    Support to read shader source from a file.

--- GameEngine/src/Platform/OpenGL/OpenGLShader.h
+++ GameEngine/src/Platform/OpenGL/OpenGLShader.h
@@ -3,9 +3,13 @@
 #include "GameEngine/Renderer/Shader.h"
 #include <glm/glm.hpp>
 
+// TODO: REMOVE!
+typedef unsigned int GLenum;
+
 namespace GE {
   class OpenGLShader : public Shader {
   public: 
+    OpenGLShader(const std::string& filepath);
     OpenGLShader(const std::string& vertexSrc, const std::string& fragmentSrc);
     virtual ~OpenGLShader();
 
@@ -21,6 +25,10 @@ namespace GE {
 
     void UploadUniformMat3(const std::string& name, const glm::mat3& matrix);
     void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
+  private:
+    std::string ReadFile(const std::string& filepath);
+    std::unordered_map<GLenum, std::string> Preprocess(const std::string& source);
+    void Compile(const std::unordered_map<GLenum, std::string>& shaderSources);
   private:
     uint32_t m_RendererID;
   };
