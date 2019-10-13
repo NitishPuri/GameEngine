6f59089bbaaf2331882f1a65768911a3dd663c42
Puri, Nitish <nitish.puri@stryker.com>
2019-10-14 01:56:51 +0530

    fix : bug in LayerStack while passing an overlay.

--- GameEngine/src/GameEngine/LayerStack.cpp
+++ GameEngine/src/GameEngine/LayerStack.cpp
@@ -26,8 +26,8 @@ namespace GE {
 
   void LayerStack::PopLayer(Layer * layer)
   {
-    auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
-    if (it != m_Layers.end()) {
+    auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex, layer);
+    if (it != m_Layers.begin() + m_LayerInsertIndex) {
       layer->OnDetach();
       m_Layers.erase(it);
       m_LayerInsertIndex--;
@@ -36,7 +36,7 @@ namespace GE {
 
   void LayerStack::PopOverlay(Layer * overlay)
   {
-    auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
+    auto it = std::find(m_Layers.begin() + m_LayerInsertIndex, m_Layers.end(), overlay);
     if (it != m_Layers.end()) {
       overlay->OnDetach();
       m_Layers.erase(it);
