f8455fb0da319cbd977ed8db95b8514c1edca498
Puri, Nitish <nitish.puri@stryker.com>
2019-11-08 02:35:11 +0530

    Combine texture and flat color shaders.

--- GameEngine/src/GameEngine/Renderer/Renderer2D.cpp
+++ GameEngine/src/GameEngine/Renderer/Renderer2D.cpp
@@ -11,8 +11,8 @@ namespace GE {
 
     struct Renderer2DStorage {
         Ref<VertexArray> QuadVertexArray;
-        Ref<Shader> FlatColorShader;
         Ref<Shader> TextureShader;
+        Ref<Texture2D> WhiteTexture;
     };
 
     static Renderer2DStorage* s_Data;
@@ -44,7 +44,10 @@ namespace GE {
         squareIB.reset(IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
         s_Data->QuadVertexArray->SetIndexBuffer(squareIB);
 
-        s_Data->FlatColorShader = Shader::Create("assets/shaders/FlatColor.glsl");
+        s_Data->WhiteTexture = Texture2D::Create(1, 1);
+        uint32_t whiteTextureData = 0xffffffff;
+        s_Data->WhiteTexture->SetData(&whiteTextureData, sizeof(uint32_t));
+
         s_Data->TextureShader = Shader::Create("assets/shaders/Texture.glsl");
         s_Data->TextureShader->Bind();
         s_Data->TextureShader->SetInt("u_Texture", 0);
@@ -58,12 +61,8 @@ namespace GE {
 
     void Renderer2D::BeginScene(const OrthographicCamera & camera)
     {
-        s_Data->FlatColorShader->Bind();        
-        s_Data->FlatColorShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
-
         s_Data->TextureShader->Bind();
         s_Data->TextureShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
-
     }
 
     void Renderer2D::EndScene()
@@ -77,13 +76,13 @@ namespace GE {
 
     void Renderer2D::DrawQuad(const glm::vec3 & position, const glm::vec2 & size, const glm::vec4 & color)
     {
-        s_Data->FlatColorShader->Bind();
-        s_Data->FlatColorShader->SetFloat4("u_Color", color);
+        s_Data->TextureShader->SetFloat4("u_Color", color);
+        s_Data->WhiteTexture->Bind();
 
         glm::mat4 transform = glm::translate(glm::mat4(1.0), position) *
             glm::scale(glm::mat4(1.0), {size.x, size.y, 1.0});
 
-        s_Data->FlatColorShader->SetMat4("u_Transform", transform);
+        s_Data->TextureShader->SetMat4("u_Transform", transform);
 
         s_Data->QuadVertexArray->Bind();
         RenderCommand::DrawIndexed(s_Data->QuadVertexArray);
@@ -96,13 +95,12 @@ namespace GE {
 
     void Renderer2D::DrawQuad(const glm::vec3 & position, const glm::vec2 & size, Ref<Texture2D>& texture)
     {
-        s_Data->TextureShader->Bind();
+        s_Data->TextureShader->SetFloat4("u_Color", glm::vec4(1.0f));
+        texture->Bind();
 
         glm::mat4 transform = glm::translate(glm::mat4(1.0), position) *
             glm::scale(glm::mat4(1.0), { size.x, size.y, 1.0 });
         s_Data->TextureShader->SetMat4("u_Transform", transform);
-
-        texture->Bind();
 
         s_Data->QuadVertexArray->Bind();
         RenderCommand::DrawIndexed(s_Data->QuadVertexArray);
