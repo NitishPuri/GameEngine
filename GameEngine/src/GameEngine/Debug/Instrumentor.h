5684f5b888cb3cba26717c6f3876d6c5b07ff306
Puri, Nitish <nitish.puri@stryker.com>
2020-03-25 20:11:45 +0530

    change _FUNC_SIG_ based on platform.

--- GameEngine/src/GameEngine/Debug/Instrumentor.h
+++ GameEngine/src/GameEngine/Debug/Instrumentor.h
@@ -140,10 +140,31 @@ namespace GE {
 
 #define GE_PROFILE 1
 #if GE_PROFILE
+    // Resolve which function signature macro will be used. Note that this only
+    // is resolved when the (pre)compiler starts, so the syntax highlighting
+    // could mark the wrong one in your editor!
+    #if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+        #define HZ_FUNC_SIG __PRETTY_FUNCTION__
+    #elif defined(__DMC__) && (__DMC__ >= 0x810)
+        #define HZ_FUNC_SIG __PRETTY_FUNCTION__
+    #elif defined(__FUNCSIG__)
+        #define HZ_FUNC_SIG __FUNCSIG__
+    #elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+        #define HZ_FUNC_SIG __FUNCTION__
+    #elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+        #define HZ_FUNC_SIG __FUNC__
+    #elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+        #define HZ_FUNC_SIG __func__
+    #elif defined(__cplusplus) && (__cplusplus >= 201103)
+        #define HZ_FUNC_SIG __func__
+    #else
+        #define HZ_FUNC_SIG "HZ_FUNC_SIG unknown!"
+    #endif
+
     #define GE_PROFILE_BEGIN_SESSION(name, filepath) ::GE::Instrumentor::Get().BeginSession(name, filepath)
     #define GE_PROFILE_END_SESSION() ::GE::Instrumentor::Get().EndSession()
     #define GE_PROFILE_SCOPE(name) ::GE::InstrumentationTimer timer##__LINE__(name);
-    #define GE_PROFILE_FUNCTION() GE_PROFILE_SCOPE(__FUNCSIG__)
+    #define GE_PROFILE_FUNCTION() GE_PROFILE_SCOPE(HZ_FUNC_SIG)
 #else
     #define GE_PROFILE_BEGIN_SESSION(name, filepath)
     #define GE_PROFILE_END_SESSION()
